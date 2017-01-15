32b9dc483eea881b34524a373f5527ebc5719d60
afcondon <andrew.condon@gmail.com>
2017-01-15 13:03:17 +0100

    single method example

--- src/D3HOAS/Selection.purs
+++ src/D3HOAS/Selection.purs
@@ -5,18 +5,23 @@ import D3HOAS.Base
 
 class Selection repr where
     d3Select    :: Selector -> repr
-    d3SelectAll :: Selector -> repr
-    select      :: Selector -> repr -> repr
-    selectAll   :: Selector -> repr -> repr
-    merge       :: repr -> repr -> repr
-    insert      :: D3ElementType -> repr -> repr
-    append      :: D3ElementType -> repr -> repr
-    remove      :: repr -> Unit
-    enter       :: repr -> repr
-    exit        :: repr -> repr
-    transition  :: D3Transition -> repr -> repr
-    attrs       :: ∀ d. Array (Attr d) -> repr -> repr
-    dataA       :: ∀ d. Array d -> repr -> repr
-    dataH       :: ∀ d. Array d -> repr -> repr
-    dataAI      :: ∀ d i. Array d -> (d -> i) -> repr -> repr
-    dataHI      :: ∀ d i. Array d -> (d -> i) -> repr -> repr
+    -- d3SelectAll :: Selector -> repr
+    -- select      :: Selector -> repr -> repr
+    -- selectAll   :: Selector -> repr -> repr
+    -- merge       :: repr -> repr -> repr
+    -- insert      :: D3ElementType -> repr -> repr
+    -- append      :: D3ElementType -> repr -> repr
+    -- remove      :: repr -> Unit
+    -- enter       :: repr -> repr
+    -- exit        :: repr -> repr
+    -- transition  :: D3Transition -> repr -> repr
+    -- attrs       :: ∀ d. Array (Attr d) -> repr -> repr
+    -- dataA       :: ∀ d. Array d -> repr -> repr
+    -- dataH       :: ∀ d. Array d -> repr -> repr
+    -- dataAI      :: ∀ d i. Array d -> (d -> i) -> repr -> repr
+    -- dataHI      :: ∀ d i. Array d -> (d -> i) -> repr -> repr
+
+data DummySelection = DS { d3Selection :: String }
+
+instance selectionDummySelection :: Selection DummySelection where
+    d3Select selector = DS { d3Selection: selector }
