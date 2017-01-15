01040856b3eed7ea25e89c9239e563abbffa76f8
afcondon <andrew.condon@gmail.com>
2017-01-15 12:54:37 +0100

    initial commit

---
+++ src/D3HOAS/Base.purs
@@ -0,0 +1,71 @@
+module D3HOAS.Base where
+
+import Prelude (Unit)
+import Color (Color)
+import DOM.Event.Types (Event)
+
+-- ||               Core foreign imports
+-- the Effect of D3
+foreign import data D3          :: !
+-- the underlying D3 selection that is passed between calls
+foreign import data D3Selection :: *
+-- a Selection that's passed back in some callbacks
+foreign import data Peers       :: *
+-- the `this` pointer in a callback, DOM element receiving an event
+foreign import data DomElement  :: *
+
+type Selector = String
+
+data Duration = Seconds Int | MS Int
+data D3Transition = SimpleTransition Duration
+                  | NamedTransition String Duration
+
+data D3ElementType
+    =     SvgCircle
+        -- | SvgEllipse
+        | SvgGroup
+        | SvgImage
+        -- | SvgLine
+        -- | SvgMesh
+        | SvgPath
+        -- | SvgPolygon
+        -- | SvgPolyline
+        | SvgRect
+        | SvgText
+        -- | SvgUse
+
+data Callback d b =   Lambda1 (d ->                                  b)
+                    | Lambda2 (d -> Number ->                        b)
+                    | Lambda4 (d -> Number -> Peers -> DomElement -> b)
+
+data ValueOrCallback d b =  V b
+                          | F (Callback d b)
+
+type SVGPathString = String -- could validate this here at least with runtime constructor
+
+data Attr d  = CX                  (ValueOrCallback d Number)  -- circles only
+             | CY                  (ValueOrCallback d Number)  -- circles only
+             | R                   (ValueOrCallback d Number)  -- circles only
+             | X                   (ValueOrCallback d Number)
+             | Y                   (ValueOrCallback d Number)
+             | DX                  (ValueOrCallback d String) -- string because it might have units with
+             | DY                  (ValueOrCallback d String)
+             | Height              (ValueOrCallback d Number)
+             | Width               (ValueOrCallback d Number)
+             | StrokeWidth         (ValueOrCallback d Number)
+             | StrokeOpacity       (ValueOrCallback d Number)
+             | FillOpacity         (ValueOrCallback d Number)
+             | Opacity             (ValueOrCallback d Number)
+             | D                   (ValueOrCallback d SVGPathString)
+             | Id                  (ValueOrCallback d String)
+             | StrokeLineCap       (ValueOrCallback d String) -- Round | Butt | Square
+             | PatternUnits        (ValueOrCallback d String) -- userSpaceOnUse | objectBoundingBox
+             | Style        String (ValueOrCallback d String)
+             | Class               (ValueOrCallback d String)
+             | Text                (ValueOrCallback d String)
+             | Type                (ValueOrCallback d String) -- images only
+             | Fill                (ValueOrCallback d Color)
+             | Stroke              (ValueOrCallback d Color)
+             | Transform    String  -- would be nice to build this up from ADT too
+             | EventHandlerS Event (Callback d String) -- click | mouseenter | mouseleave | mouseover
+             | EventHandlerN Event (Callback d Number)
---
+++ src/D3HOAS/Selection.purs
@@ -0,0 +1,22 @@
+module D3HOAS.Selection where
+
+import Prelude (Unit)
+import D3HOAS.Base
+
+class Selection repr where
+    d3Select    :: Selector -> repr
+    d3SelectAll :: Selector -> repr
+    select      :: Selector -> repr -> repr
+    selectAll   :: Selector -> repr -> repr
+    merge       :: repr -> repr -> repr
+    insert      :: D3ElementType -> repr -> repr
+    append      :: D3ElementType -> repr -> repr
+    remove      :: repr -> Unit
+    enter       :: repr -> repr
+    exit        :: repr -> repr
+    transition  :: D3Transition -> repr -> repr
+    attrs       :: ∀ d. Array (Attr d) -> repr -> repr
+    dataA       :: ∀ d. Array d -> repr -> repr
+    dataH       :: ∀ d. Array d -> repr -> repr
+    dataAI      :: ∀ d i. Array d -> (d -> i) -> repr -> repr
+    dataHI      :: ∀ d i. Array d -> (d -> i) -> repr -> repr
