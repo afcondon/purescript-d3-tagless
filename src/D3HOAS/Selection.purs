431e902c0c7c14ab126df64433f08304c4453c1b
afcondon <andrew.condon@gmail.com>
2017-01-25 21:32:08 +0100

    finish monad instance

--- src/D3HOAS/Selection.purs
+++ src/D3HOAS/Selection.purs
@@ -1,10 +1,28 @@
 module D3HOAS.Selection where
 
-import Prelude (Unit)
-import D3HOAS.Base
+import D3HOAS.Base (Selector)
+import Prelude (class Applicative, class Apply, class Bind, class Functor, class Monad, Unit, apply, bind, pure, unit, ($), ap)
 
-class Selection repr where
-    d3Select    :: Selector -> repr
+data FakeSelection a = FakeSelection a -- could have EmptySelection here too?
+
+instance functorFakeSelection :: Functor FakeSelection where
+  map f (FakeSelection s) = FakeSelection $ f s
+
+instance applyFakeSelection :: Apply FakeSelection where
+  apply = ap
+
+instance applicativeFakeSelection :: Applicative FakeSelection where
+  pure a = FakeSelection a
+
+instance bindFakeSelection :: Bind FakeSelection where
+  bind (FakeSelection a) f = f a
+
+instance monadFakeSelection :: Monad FakeSelection
+
+
+
+class (Monad repr) <= Selection repr where
+    d3Select    :: Selector -> repr Unit
     -- d3SelectAll :: Selector -> repr
     -- select      :: Selector -> repr -> repr
     -- selectAll   :: Selector -> repr -> repr
@@ -21,7 +39,7 @@ class Selection repr where
     -- dataAI      :: ∀ d i. Array d -> (d -> i) -> repr -> repr
     -- dataHI      :: ∀ d i. Array d -> (d -> i) -> repr -> repr
 
-data DummySelection = DS { d3Selection :: String }
-
-instance selectionDummySelection :: Selection DummySelection where
-    d3Select selector = DS { d3Selection: selector }
+-- data DummySelection a = DS { d3Selection :: String }
+--
+-- instance selectionDummySelection :: Selection DummySelection where
+--     d3Select selector = DS { d3Selection: selector }
