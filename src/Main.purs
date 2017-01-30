411f872f86a4121601cc60bb310c914eb76cd421
afcondon <andrew.condon@gmail.com>
2017-01-30 18:43:27 +0100

    add example of index function on data
    also commented out the sigs, both to show type inference isn’t necessary for them and to make it look even more JavaScript like (just to show that’s possible, ie not adding lots of overhead to creation of D3 scripts)

--- src/Main.purs
+++ src/Main.purs
@@ -2,8 +2,10 @@ module Main where
 
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log, logShow)
+import Data.Int (toNumber)
 import Data.Maybe (Maybe(..))
 import Data.Monoid (mempty)
+import Number (divide)
 import Prelude (Unit, bind, id, ($))
 import TaglessD3.Base (Attr(..), D3ElementType(..), D3Transition(..), Duration(..), ValueOrCallback(..), (..))
 import TaglessD3.DOMImpl (runStructure, D3Structure(..)) as D
@@ -16,16 +18,22 @@ d3Script' = d3Select "#chart"
          .. enter
          .. append SvgText
          .. attrs [ CX (V 2.0)] -- , CY (V "45px") ]
-         .. transition (NamedTransition "t1" (MS 500))
+         .. transition myTransition
 
-myData :: D3Data Int Int
+-- myTransition :: D3Transition
+myTransition = NamedTransition "t1" $ MS 500
+
+-- myData' :: D3Data Int Number
+myData' = ArrayD [1,2,3,4,5] (\i -> divide (toNumber i) 2.0)
+
+-- myData :: D3Data Int Int
 myData = ArrayD [1,2,3,4,5] id
 
-myD3Selection :: D.D3Structure Int Int
+-- myD3Selection :: D.D3Structure Int Int
 myD3Selection = (D.D3S { selection: Nothing, "data": Nothing })
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
     logShow $ S.runStructure d3Script' mempty
-    log "\n\n\ncool beans\n\n\n"
+    log "\n\n\n====== cool beans =======\n\n\n"
     logShow $ D.runStructure d3Script' myD3Selection
